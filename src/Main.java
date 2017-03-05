db4201a7e2cbda40509b3f8f3d4df026fd073ae3
LoadV <1409125153@qq.com>
2017-03-05 20:07:19 +0800

    IDEA git Test

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.io.FileOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
+import com.google.gson.Gson;
 
 public class Main
 {
 
 	public static void main(String[] args) {
-		
+		Gson gson = new Gson();
+		Student student = new Student();
+		student.name = "";
+		student.content="";
+		System.out.println( gson.toJson(student));
 	}
+
+}
+class Student
+{
+	String name;
+	String content;
 }
 
